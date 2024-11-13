e335bdab988f9fea076fa162cb99c112929c9bc0
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-12 20:08:17 -0500

    basic types

--- com.rsk/Demo.kt
+++ com.rsk/Demo.kt
@@ -2,6 +2,7 @@ package com.rsk
 
 var num: Int = 3
 val total = 45
+val longNumber: Long = 3_000_000_000
 
 fun main(args: Array<String>) {
     for (a in args) {
@@ -12,6 +13,8 @@ fun main(args: Array<String>) {
     println(sum2(1, 1))
     println(num)
     println(total)
+    println(longNumber)
+    println(48/23.toDouble())
 }
 
 fun sum(a: Int, b: Int): Int {
