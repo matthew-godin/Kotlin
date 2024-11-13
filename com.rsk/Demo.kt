3efd687000e8ad272f0f82fc07da01f4d8884f79
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-12 19:59:31 -0500

    functions

--- com.rsk/Demo.kt
+++ com.rsk/Demo.kt
@@ -4,4 +4,13 @@ fun main(args: Array<String>) {
     for (a in args) {
         println(a)
     }
+    println()
+    println(sum(32, 24))
+    println(sum2(1, 1))
 }
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum2(a: Int, b: Int): Int = a + b
