62f767e89d358894fc6446a4e79cc2298a7b2044
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-15 17:55:49 -0500

    extension functions

--- com.rsk/Functions.kt
+++ com.rsk/Functions.kt
@@ -1,7 +1,22 @@
 package com.rsk
 
+// sinlge expression function
 fun max(a: Int, b: Int) : Int = if (a > b) a else b
+
+// extension function
+fun String.pad(numberToReplace: Int, replacementChar: Char = ' '): String {
+    val strToAdd = replacementChar.toString().repeat(numberToReplace)
+    return this + strToAdd
+}
 
 fun main() {
     println(max(4, 3))
+
+    val original = "Kevin"
+
+    val newerString = original.pad(4, '*')
+
+    println(newerString)
+
+    println(Point(4, 5) add Point(3, 6))
 }
