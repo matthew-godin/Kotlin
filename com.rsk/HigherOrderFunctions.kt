fd3c686449b99c9c49a39643052836503182442f
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-15 18:27:51 -0500

    higher order functions (similar to JavaScript arrow functions)

---
+++ com.rsk/HigherOrderFunctions.kt
@@ -0,0 +1,59 @@
+package com.rsk
+
+interface Process {
+    fun execute(value: Int)
+}
+
+fun main() {
+    fibonacci(8)
+    fibonacci2(8, object : Process {
+        override fun execute(value: Int) {
+            println(value)
+        }
+    })
+    fibonacci3(8, {n -> println(n)})
+}
+
+// strategy pattern using higher order function 
+fun fibonacci3(limit: Int, action: (Int) -> Unit) {
+    var prev = 0
+    var prevPrev = 0
+    var current = 1
+     
+     for (i in 1..limit) {
+        action(current)
+        var temp = current
+        prevPrev = prev
+        prev = temp
+        current = prev + prevPrev
+     }
+}
+
+// strategy pattern using interface
+fun fibonacci2(limit: Int, process: Process) {
+    var prev = 0
+    var prevPrev = 0
+    var current = 1
+     
+     for (i in 1..limit) {
+        process.execute(current)
+        var temp = current
+        prevPrev = prev
+        prev = temp
+        current = prev + prevPrev
+     }
+}
+
+fun fibonacci(limit: Int) {
+    var prev = 0
+    var prevPrev = 0
+    var current = 1
+     
+     for (i in 1..limit) {
+        println(current)
+        var temp = current
+        prevPrev = prev
+        prev = temp
+        current = prev + prevPrev
+     }
+}
