4dd084c96c7069fe4237be1761c5f1fadc24044e
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-16 18:45:21 -0500

    contravariance using 'in' keyword

--- com.rsk/Generics.kt
+++ com.rsk/Generics.kt
@@ -2,7 +2,7 @@ import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
     //val env: Envelope<Person> = Envelope(Person(), null)
-    val env: Envelope<List<Person>> = Envelope(listOf(Person(), Person()), null)
+    //val env: Envelope<List<Person>> = Envelope(listOf(Person(), Person()), null)
 }
 
 class Envelope<T>(val result: T?, val message: String?) where T:List<Any?> {
@@ -10,6 +10,29 @@ class Envelope<T>(val result: T?, val message: String?) where T:List<Any?> {
 
     init {
         timeGenerated = LocalDateTime.now()
+    }
+}
+
+interface Consumer<in T> {
+    fun consume(data: T)
+}
+
+fun useConsumer(personConsumer: Consumer<Person>) {
+    val studentConsumer: Consumer<Student> = StudentConsumer()
+    studentConsumer.consume(Student(1))
+    personConsumer.consume(Person(""))
+    personConsumer.consume(Student(1))
+}
+
+class PersonConsumer : Consumer<Person> {
+    override fun consume(data: Person) {
+
+    }
+}
+
+class StudentConsumer : Consumer<Student> {
+    override fun consume(data: Student) {
+
     }
 }
 
@@ -22,6 +45,6 @@ interface Producer<out T> {
     fun next() : T
 }
 
-open class Person
+open class Person(val name: String)
 
-class Student : Person( )
+class Student(val studentId: Int) : Person("")
