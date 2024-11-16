182047abdb721e158628dc22565895ac5f4243ae
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-16 18:11:35 -0500

    force a certain kind of type using 'where' keyword

--- com.rsk/Generics.kt
+++ com.rsk/Generics.kt
@@ -1,10 +1,11 @@
 import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
-    val env: Envelope<Person> = Envelope(Person(), null)
+    //val env: Envelope<Person> = Envelope(Person(), null)
+    val env: Envelope<List<Person>> = Envelope(listOf(Person(), Person()), null)
 }
 
-class Envelope<T>(val result: T?, val message: String?) {
+class Envelope<T>(val result: T?, val message: String?) where T:List<Any?> {
     val timeGenerated: LocalDateTime
 
     init {
