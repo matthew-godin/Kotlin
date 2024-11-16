f3e818a17b786cd9adb014c20aac9de54a1d410f
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-16 12:35:06 -0500

    Kotlin safe cast

--- com.rsk/Nullability.kt
+++ com.rsk/Nullability.kt
@@ -7,11 +7,19 @@ fun main() {
     // name = null
     name = "George"
 
-    val name2: String?
+    var name2: String?
 
     name2 = null
 
     val name3: String = name2 ?: name
 
     println(name3)
+
+    val num: Int = 33
+
+    // will get null as Int cannot be cast to String
+    // normal 'as' would throw an exception
+    name2 = num as? String
+
+    println(name2)
 }
