04a87103d46c8a294b3cc13cf7798c1ae6d7f656
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-11-15 20:25:50 -0500

    collections (lists and hashmaps)

--- com.rsk/CollectionsAndSequences.kt
+++ com.rsk/CollectionsAndSequences.kt
@@ -2,9 +2,17 @@ package com.rsk
 
 fun main() {
     var i = 0
-    val numbers = Array<Int>(5) { i++ }
+    //val numbers = Array<Int>(5) { i++ }
+
+    val numbers = listOf(1, 3, 2)
 
     for (num in numbers) {
         println(num)
+    }
+
+    val people = mapOf<Int, String>(1 to "One", 2 to "Two")
+
+    for ((i, v) in people) {
+        println("$i -> $v")
     }
 }
